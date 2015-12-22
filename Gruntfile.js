cf6aa0fc7388a95066265bfcbf54731ae7850323
Edwin Kalkman <kalkman@gmail.com>
2015-12-22 16:59:44 +0100

    chore(setup): grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+	'use strict';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: './js/source',
+					mainConfigFile: './js/source/main.js',
+					name: 'main',
+					findNestedDependencies: true,
+					optimize: 'none',
+					preserveLicenseComments: false,
+					out: './js/build/main.js',
+					paths: {
+						'jquery': 'empty:',
+						'highcharts': 'empty:'
+					}
+				}
+			}
+		},
+		sass: {
+			files: {
+				expand: true,
+				cwd: './css/scss',
+				src: ['**/*.scss'],
+				dest: './css',
+				ext: '.css'
+			}
+		},
+		watch: {
+			js: {
+				files:  './js/source/**/*.js',
+				tasks: ['requirejs'],
+				options:  {
+					livereload: true,
+					spawn: false
+				}
+			},
+			sass: {
+				files: './css/scss/**/*.scss',
+				tasks: ['sass'],
+				options: {
+					livereload: true,
+					spawn: false
+				}
+			}
+		}
+	});
+
+	// Load installed tasks from packsage.json
+	require('load-grunt-tasks')(grunt);
+
+	// Display the elapsed execution time of grunt tasks
+	require('time-grunt')(grunt);
+
+	// Default task
+	grunt.registerTask('default', ['sass']);
+};
